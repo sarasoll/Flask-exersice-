45d33e614bfe1e0b12e050834ab5bcf989c77bb6
Soll✯ <jack-soll@live.com>
2023-03-21 14:16:35 +0300

    add some commites to the code

--- sara/solo/app.py
+++ sara/solo/app.py
@@ -2,6 +2,23 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+done = {}
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/register", methods=["POST"])
+def register():
+    myname = request.form.get("myname")
+    myvipe = request.form.get("myvipe")
+    done[myname] = myvipe
+    return render_template("register.html")
+
+@app.route("/done")
+def done():
+    return render_template("done.html", done=done)
+
+
+if __name__ == "__main__":
+    app.run()
